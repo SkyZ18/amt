0fed4bdf22fa22b0f87e35e8befedea952fed3c5
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2023-10-05 10:29:10 +0200

    Configured Security

--- article_api/src/main/java/com/api/article_api/config/SecurityConfig.java
+++ article_api/src/main/java/com/api/article_api/config/SecurityConfig.java
@@ -1,19 +1,36 @@
 package com.api.article_api.config;
 
+import com.api.article_api.security.JwtAuthenticationEntryPoint;
+import com.api.article_api.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
--- article_api/src/main/java/com/api/article_api/security/JwtTokenProvider.java
+++ article_api/src/main/java/com/api/article_api/security/JwtTokenProvider.java
@@ -1,13 +1,21 @@
 package com.api.article_api.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.api.article_api.models.UserModel;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+@Component
+@Slf4j
 public class JwtTokenProvider {
+
+    @Value("${app.jwtSecret}")
     private String jwtSecret;
 
     public String generateToken(String userEmail) {
@@ -22,7 +30,37 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public boolean validateToken(String token) {
+    public String generateToken(Authentication authentication) {
+        UserModel userModel = (UserModel) authentication.getPrincipal();
+        return generateToken(userModel.getEmail());
+    }
 
+    public String getEmailFromToken(String token) {
+        Claims claims = Jwts
+                .parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            log.error("Token Invalid");
+        } catch (ExpiredJwtException ex) {
+            log.error("Token Expired");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Token Unsupported");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty");
+        }
+
+        return false;
     }
 }
